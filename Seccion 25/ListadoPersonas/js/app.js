08d41face186ccb2e836d7de4a1b410d7468dc85
JuanaGonzalez21 <juanita.juana@hotmail.com>
2024-04-03 23:08:04 -0500

    Listado  de personas proyecto)

--- Seccion 25/ListadoPersonas/js/app.js
+++ Seccion 25/ListadoPersonas/js/app.js
@@ -1,1 +1,17 @@
-console.log('Hola')
+//Arreglo para acceder desde cualquier parte
+
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Carla', 'Lara')
+]
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...')
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`
+    }
+
+    document.getElementById('personas').innerHTML = texto
+}
